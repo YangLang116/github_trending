33ff9f395ef620a2ac73e841a69c6a17f37325d7
GitHub Action <action@github.com>
2024-02-19 01:42:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/yanglang116/githubtrending/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yanglang116/githubtrending/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.yanglang116.githubtrending.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yanglang116.githubtrending.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
